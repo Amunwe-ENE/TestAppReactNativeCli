3b9621b094deb1ceb72e6c882118e5fec76f4e76
amunwe-ene <eneamunwe@gmail.com>
2020-03-13 20:52:21 +0200

    fixed navigations

--- SignIn.js
+++ SignIn.js
@@ -10,7 +10,7 @@ export default class SignIn extends React.Component {
     firebase
       .auth()
       .signInWithEmailAndPassword(email, password)
-      .then(() => this.props.navigation.navigate('Main'))
+      .then(() => this.props.navigation.navigate('Form'))
       .catch(error => this.setState({ errorMessage: error.message }))
   }
 
